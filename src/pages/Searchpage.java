27aca93e6287705dc262aa3abe02f7d8ee1269df
pavanredddysomula <somulapavan127@gmail.com>
2023-06-15 19:24:37 +0530

    added/deleted/modify

---
+++ src/pages/Searchpage.java
@@ -0,0 +1,10 @@
+package pages;
+
+public class Searchpage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/tests/LoginPagetest.java
+++ src/tests/LoginPagetest.java
@@ -2,4 +2,10 @@ package tests;
 
 public class LoginPagetest {
 
-}
+	public static void main(String[] args) {
+		System.out.println("Test1 loginpahe");
+	}
+
+	}
+
+
