e0e62aaf226e0a0c73210555a55922b758f37ec2
pavanredddysomula <somulapavan127@gmail.com>
2023-06-15 19:35:30 +0530

    modified pages classes

--- src/tests/LoginPagetest.java
+++ src/tests/LoginPagetest.java
@@ -3,7 +3,12 @@ package tests;
 public class LoginPagetest {
 
 	public static void main(String[] args) {
-		System.out.println("Test1 loginpahe");
+		System.out.println("Test1 loginpage");
+		System.out.println("Hello pavan");
+		String un="admin";
+		String pwd="admin";
+		System.out.println("logged In..");
+		System.out.println("logout");
 	}
 
 	}
